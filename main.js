251a3c4531b61b7e8f0008d10f445e4a24c7e99f
Pakapol Tanachaisith <pakapol.tanachaisith@gmail.com>
2024-03-03 11:34:15 +0700

    Build 16 X 16 grid

---
+++ main.js
@@ -0,0 +1,20 @@
+const container = document.querySelector("#container");
+
+function createRow(perRow = 16) {
+  const row = document.createElement("div");
+  row.classList.add("row");
+
+  for (let i = 1; i <= perRow; i++) {
+    const rowItem = document.createElement("div");
+    rowItem.classList.add("row-item");
+    rowItem.textContent = i;
+
+    row.appendChild(rowItem);
+  }
+
+  return row;
+}
+
+for (let i = 1; i <= 16; i++) {
+  container.appendChild(createRow());
+}
