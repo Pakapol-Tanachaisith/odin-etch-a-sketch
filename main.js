9a748bce701024d9ed9cb466c7240660a99d6d67
Pakapol Tanachaisith <pakapol.tanachaisith@gmail.com>
2024-03-03 12:17:41 +0700

    Add ability to set dimension of the grid

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
 const container = document.querySelector("#container");
 
-function createRow(perRow = 16) {
+// Generate grid
+function createRow(width) {
   const row = document.createElement("div");
   row.classList.add("row");
 
-  for (let i = 1; i <= perRow; i++) {
+  for (let i = 1; i <= width; i++) {
     const rowItem = document.createElement("div");
     rowItem.classList.add("row-item");
-    rowItem.textContent = i;
 
     rowItem.addEventListener("mouseover", () => {
       rowItem.style.backgroundColor = "black";
@@ -19,6 +19,29 @@ function createRow(perRow = 16) {
   return row;
 }
 
-for (let i = 1; i <= 16; i++) {
-  container.appendChild(createRow());
+function createGrid(width = 16, height = 16) {
+  while (container.firstChild) {
+    container.removeChild(container.firstChild);
+  }
+
+  for (let i = 1; i <= height; i++) {
+    container.appendChild(createRow(width));
+  }
 }
+
+// Dimension
+function setDimension() {
+  const selectedWidth = prompt("Select width (max: 100px):");
+  const newWidth = selectedWidth <= 100 ? selectedWidth : 100;
+
+  const selectedHeight = prompt("Select Height (max: 100px):");
+  const newHeight = selectedHeight <= 100 ? selectedHeight : 100;
+
+  createGrid(newWidth, newHeight);
+}
+
+const dimensionBtn = document.querySelector("#dimension-btn");
+dimensionBtn.addEventListener("click", setDimension);
+
+// Create initial grid
+createGrid();
